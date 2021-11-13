472d56272588d8fc6648b26f32455a2462339579
pobats <pobat.stanislav@ya.ru>
2021-11-13 20:47:43 +0300

    lesson 4

---
+++ GB_CODE_41/GB_CODE_41/less41.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// 
+/// 1.	Написать метод GetFullName(string firstName, string lastName, string patronymic), 
+/// принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО. 
+/// Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+/// 
+
+{
+    int n = 4; //количество участников опроса
+    var contact = new string[n]; // список конатктов
+        for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"Заполните анкету для учатсника № {i+1} из {n}");
+        contact[i] = GetFullName();
+        // Console.WriteLine(GetFullName());
+    }
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"{contact [i]}");
+    }
+}
+static string GetFullName()
+{
+    Console.WriteLine("Введи свою Фамилию:");
+    string firstName = Console.ReadLine();
+    Console.WriteLine("Введи свое Имя:");
+    string lastName = Console.ReadLine();
+    Console.WriteLine("Введи свое Имя:");
+    string patronymic = Console.ReadLine();
+    //string FIO = firstName + " " + lastName + " " + patronymic;
+    //return FIO;
+    return firstName + " " + lastName + " " + patronymic;
+}
+
---
+++ GB_CODE_44/GB_CODE_44/less44.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//4.	(*) Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом
+
+namespace HelloWorld
+{
+    class less44
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Getfib(9)); // 9 - циклов метода 
+        }
+
+
+        static int Getfib(int number)
+        {
+            if (number == 0)
+                return 0; // певое число
+            if (number == 1) // второе число
+                return 1;
+
+            return Getfib(number - 1) + Getfib(number - 2); //
+        }
+    }
+}
