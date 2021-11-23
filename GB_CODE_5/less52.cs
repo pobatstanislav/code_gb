01a0848865b9a0dc5c97070c629cb4d996f7dd40
pobats <pobat.stanislav@ya.ru>
2021-11-23 17:01:36 +0300

    lesson 5

---
+++ GB_CODE_5/less52.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.IO;
+
+//2.	Написать программу, которая при старте дописывает текущее время в файл «startup.txt».
+string myTime = System.DateTime.Now.ToString("yyyy.MM.dd_HH.mm");
+//Console.WriteLine(myTime);
+File.AppendAllText("startup.txt", myTime + Environment.NewLine);
+
+
